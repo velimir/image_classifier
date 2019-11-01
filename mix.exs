ef5b1d19455985988a4de160fb64151e1d724892
Grigory Starinkin <starinkin@gmail.com>
2019-11-01 14:34:49 +0000

    add package description and post link

--- mix.exs
+++ mix.exs
@@ -4,13 +4,17 @@ defmodule ImageClassifier.MixProject do
   def project do
     [
       app: :image_classifier,
+      description: "Use Tensorflow for image classification in Elixir",
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       package: [
         maintainers: ["Grigory Starinkin"],
         licenses: ["MIT"],
-        links: %{"GitHub" => "https://github.com/velimir/image_classifier"}
+        links: %{
+          "GitHub" => "https://github.com/velimir/image_classifier",
+          "Post" => "https://www.erlang-solutions.com/blog/how-to-build-a-machine-learning-project-in-elixir.html"
+        }
       ],
       deps: deps()
     ]
